56441c17ab78d1ff68e7fed579c79035e58cae93
mafe-rnanda <mf.chavezminjarez@gmail.com>
2021-04-07 18:07:57 -0700

    Arrange footer layout and add provisional styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,19 @@
+import "./Footer.scss";
+
 export default function Footer() {
   return (
-    <div>
-      <div>MARIA CHAVEZ</div>
-      <div>
-        <div>HOME | WORK | ABOUT | CONTACT</div>
-        <div>
-          <div>SOCIAL</div>
-          <div>
-            <p>LinkedIn</p>
-            <p>GitHub</p>
-          </div>
+    <div className="footer">
+      <h3>MARIA CHAVEZ</h3>
+      <div className="footer-links">
+        <div className="pages">
+          <p>HOME</p>
+          <p>WORK</p>
+          <p>ABOUT</p>
+          <p>CONTACT</p>
+        </div>
+        <div className="social">
+          <p>LinkedIn</p>
+          <p>GitHub</p>
         </div>
       </div>
     </div>
