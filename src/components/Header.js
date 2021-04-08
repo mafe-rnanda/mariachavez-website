700059840b91ec7be09af2247b5c31f2735a6444
mafe-rnanda <mf.chavezminjarez@gmail.com>
2021-04-07 21:07:25 -0700

    Implement opening and closing of Menu button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,28 @@
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./Header.scss";
+
 export default function Header(props) {
+  console.log("Header props: ", props);
   return (
     <div>
-      <div>Menu</div>
+      {!props.openMenu && (
+        <div
+          className="menu-button"
+          open={props.openMenu}
+          onClick={() => props.setOpenMenu(!props.openMenu)}
+        >
+          <div className="button-clicked-open">MENU</div>
+        </div>
+      )}
+      {props.openMenu && (
+        <div
+          className={"close-button"}
+          open={props.openMenu}
+          onClick={() => props.setOpenMenu(!props.openMenu)}
+        >
+          <div className="button-close">CLOSE</div>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,21 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import About from "./About";
+import Contact from "./Contact";
+import Work from "./Work";
+import "./Menu.scss";
+
 export default function Menu(props) {
+  console.log("Menu props: ", props);
   return (
-    <div>
-      <div>WORK</div>
-      <div>ABOUT</div>
-      <div>CONTACT</div>
+    <div
+      className={
+        "menu-section " + (props.openMenu ? "open-menu" : "closed-menu")
+      }
+      open={props.open}
+    >
+      <h1>WORK</h1>
+      <h1>ABOUT</h1>
+      <h1>CONTACT</h1>
     </div>
   );
 }
