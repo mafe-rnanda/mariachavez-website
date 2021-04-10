f37e0e0b056442745cbb307f5a48c59252a16f1a
mafe-rnanda <mf.chavezminjarez@gmail.com>
2021-04-09 22:24:44 -0700

    Fix margins, add hove state to menu elements

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,10 @@ import "./Header.scss";
 
 export default function Header(props) {
   console.log("Header props: ", props);
-  return (
-    <div>
-      {!props.openMenu && (
+
+  const MenuButton = () => {
+    if (!props.openMenu) {
+      return (
         <div
           className="menu-button"
           open={props.openMenu}
@@ -13,16 +14,42 @@ export default function Header(props) {
         >
           <div className="button-clicked-open">MENU</div>
         </div>
-      )}
-      {props.openMenu && (
+      );
+    } else {
+      return (
         <div
-          className={"close-button"}
+          className="close-button"
           open={props.openMenu}
           onClick={() => props.setOpenMenu(!props.openMenu)}
         >
           <div className="button-close">CLOSE</div>
         </div>
-      )}
-    </div>
+      );
+    }
+  };
+
+  return (
+    // <div>
+    //   {!props.openMenu && (
+    //     <div
+    //       className="menu-button"
+    //       open={props.openMenu}
+    //       onClick={() => props.setOpenMenu(!props.openMenu)}
+    //     >
+    //       <div className="button-clicked-open">MENU</div>
+    //     </div>
+    //   )}
+    //   {props.openMenu && (
+    //     <div
+    //       className={"close-button"}
+    //       open={props.openMenu}
+    //       onClick={() => props.setOpenMenu(!props.openMenu)}
+    //     >
+    //       <div className="button-close">CLOSE</div>
+    //     </div>
+    //   )}
+    // </div>
+
+    MenuButton()
   );
 }
