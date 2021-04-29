be70919e0720729d6bd12528ccacd1a994ef2b93
Maria <mfchaavez@gmail.com>
2021-04-28 20:19:09 -0700

    Add links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,36 @@
 import "./Footer.scss";
 
 export default function Footer() {
-  return (
-    <div className="footer">
-      <h3>MARIA CHAVEZ</h3>
-      <div className="footer-links">
-        <div className="pages">
-          <p>HOME</p>
-          <p>WORK</p>
-          <p>ABOUT</p>
-          <p>CONTACT</p>
+    return (
+        <div className="footer">
+            <h3>MARIA CHAVEZ</h3>
+            <div className="footer-links">
+                <div className="pages">
+                    <p>
+                        <a href="/">HOME</a>
+                    </p>
+                    <p>
+                        <a href="/work">WORK</a>
+                    </p>
+
+                    <p>
+                        <a href="/about">ABOUT</a>
+                    </p>
+
+                    <p>
+                        <a href="/contact">CONTACT</a>
+                    </p>
+
+                </div>
+                <div className="social">
+                    <p>
+                        <a target="_blank" href="https://www.linkedin.com/in/mariachavezm">LinkedIn</a>
+                    </p>
+                    <p>
+                        <a target="_blank" href="https://github.com/mafe-rnanda">GitHub</a>
+                    </p>
+                </div>
+            </div>
         </div>
-        <div className="social">
-          <p>LinkedIn</p>
-          <p>GitHub</p>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
