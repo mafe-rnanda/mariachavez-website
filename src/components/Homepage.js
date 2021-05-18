1d446104723bc08389c815e5ba17c80670c23abf
Maria Fernanda Chavez <43701042+mafe-rnanda@users.noreply.github.com>
2021-05-17 17:20:30 -0700

    Add page links and restyle header fonts

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,16 +1,21 @@
 // import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 // import Menu from "./Menu";
+import React, { useState } from "react";
 import "./Homepage.scss";
 
 export default function Homepage() {
+  const [hovered, setHovered] = useState(false);
+  const toggleHover = () => setHovered(!hovered);
+
   return (
     <div>
       <div className="hello-intro">
-        <h1 className="greeting-hello">
-          HI. HELLO.
+        <h1 className={hovered ? "remove-greeting" : "greeting-hello"}>
+          HI, HELLO.
           <br></br>
           HOW ARE YOU?
         </h1>
+        <h1 className={hovered ? "greeting-hello" : "remove-greeting"}>WORK</h1>
         <h1 className="greeting-name">
           MY NAME IS <br></br> MARIA CHAVEZ.
         </h1>
@@ -18,9 +23,25 @@ export default function Homepage() {
           I am a junior full-stack web developer based in Vancouver.
           <br></br>
           <br></br>
-          Welcome to my page – click around and make yourself at home. If you
-          would like to talk, get to know each other and/or discuss a specific
-          project, please send me a message
+          Welcome to my page – click around and make yourself at home.{" "}
+          <a
+            href="/work"
+            className="work-link"
+            onMouseEnter={toggleHover}
+            onMouseLeave={toggleHover}
+          >
+            Here
+          </a>{" "}
+          are some of my projects and over{" "}
+          <a href="/about" className="about-link">
+            there
+          </a>{" "}
+          a bit about myself. <br></br>If you would like to talk and discuss a
+          specific project, please{" "}
+          <a href="/contact" className="contact-link">
+            send me a message
+          </a>{" "}
+          💌
           <br></br>
           <br></br>
           Enjoy.
@@ -31,7 +52,7 @@ export default function Homepage() {
           <h1>
             SOME OF MY LATEST <br></br>PROJECTS
           </h1>
-          <a>Check out more</a>
+          <a href="/work">Check out more →</a>
         </div>
         <div className="projects-grid">
           <div>
