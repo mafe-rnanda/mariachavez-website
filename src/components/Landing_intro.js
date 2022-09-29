204ca454e410b0ecb1b9f83b22e5302f3841ff7d
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2022-09-29 14:56:05 -0700

    rename files to match with app content

---
+++ src/components/Landing_intro.js
@@ -0,0 +1,17 @@
+import "./Landing_intro.scss";
+
+export default function Homepage() {
+  return (
+    <div id="landing-section">
+      <div>
+        <h1>MARIA CHAVEZ</h1>
+        <h3>Full Stack Web Developer</h3>
+      </div>
+      <p>Hello!<br/>
+        My name is Maria Chavez. <br/>
+        I am a full-stack web developer based in Canada. <br/>
+        More <a>about me</a>.</p>
+      <div></div>
+    </div>
+  );
+}
---
+++ src/components/Projects.js
@@ -0,0 +1,20 @@
+export default function Work(props) {
+  return (
+    <div>
+      <h1>PROJECTS</h1>
+      <div class="project-container">
+        <div>
+          <div>PEOJECT NAME</div>
+          <a>more details</a>
+        </div>
+      </div>
+      <div class="project-container">
+        <div>
+          <div>PEOJECT NAME</div>
+          <a>more details</a>
+        </div>
+      </div>
+      
+    </div>
+  );
+}
