6324079803946ee308429dcff40369ee7f4ce1f6
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2022-09-30 12:58:17 -0700

    add styling to landing section

--- src/components/Landing_intro.js
+++ src/components/Landing_intro.js
@@ -3,15 +3,19 @@ import "./Landing_intro.scss";
 export default function Homepage() {
   return (
     <div id="landing-section">
-      <div>
-        <h1>MARIA CHAVEZ</h1>
-        <h3>Full Stack Web Developer</h3>
-      </div>
-      <p>Hello!<br/>
+      <p class="intro-text">Hello!<br/>
         My name is Maria Chavez. <br/>
         I am a full-stack web developer based in Canada. <br/>
         More <a>about me</a>.</p>
-      <div></div>
+      <div class="intro-portrait">
+        <div class="placeholder-pic"></div>
+        <div class="rectangle"></div>
+      </div>
+      <div class="intro-header">
+        <h3>Full Stack Web Developer</h3>
+        <h1>MARIA CHAVEZ</h1>
+      </div>
+      <p class="brief-description">I seek to create efficient, aesthetic and fun websites that solves user problems through effective solutions, blah blah blah....</p>
     </div>
   );
 }
