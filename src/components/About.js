abe6a1b305c4fb9699b73fc204c5c0c59d80bc9d
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2022-10-05 21:51:14 -0700

    Add content and grid layout to about section

--- src/components/About.js
+++ src/components/About.js
@@ -2,17 +2,12 @@ import "./About.scss";
 
 export default function About(props) {
   return (
-    <div>
+    <div id="about-section">
       <h1>ABOUT ME</h1>
-      <div>
-        <p></p>
-        <div>
-          <img></img>
-          <svg></svg>
-        </div>
-      </div>
-      
-      
+      <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div>
+      <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </div>
+      <div class="placeholder-pic"></div>
+      <div class="rectangle"></div>
     </div>
   );
 }
