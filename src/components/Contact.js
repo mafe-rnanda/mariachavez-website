5534cdc667ea469a0d811f522be592dc171e1aae
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2022-10-07 13:42:10 -0700

    add links to contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,12 +9,13 @@ export default function Contact(props) {
         <a>Let's get in touch</a>
       </div>
       <div class="email-phone">
-        <a>mf.chavezminjarez@gmail.com</a><br/>
+        <a href="mailto:mf.chavezminjarez@gmail.com">mf.chavezminjarez@gmail.com</a><br/>
         <a>+1 (604) 836 3823</a>
       </div>
-      <a>GITHUB</a>
-      <a>LINKEDIN</a>
-      <a>MY RESUME</a>
+      <a href="https://github.com/mafe-rnanda" target="_blank">GITHUB</a>
+      <a href="http://www.linkedin.com/in/mariachavezm" target="_blank">LINKEDIN</a>
+      {/* <a href="https://mafe-rnanda.github.io/Resume/" target="_blank">MY RESUME</a> */}
+      <a href="https://github.com/mafe-rnanda/Resume/raw/main/MariaChavez_Resume.pdf" target="_blank">MY RESUME</a>
     </div>
   );
 }
