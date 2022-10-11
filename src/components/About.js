4ec47b9b373771c720a659ef1eb46f8676de9ad5
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2022-10-11 12:55:14 -0700

    add grid layout, add margins and sizing to about section

--- src/components/About.js
+++ src/components/About.js
@@ -3,11 +3,11 @@ import "./About.scss";
 export default function About(props) {
   return (
     <div id="about-section">
-      <h1>ABOUT ME</h1>
-      <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div>
-      <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </div>
-      <div class="placeholder-pic"></div>
-      <div class="rectangle"></div>
+      <h1 class="header">ABOUT ME</h1>
+      <div class="introduction highlight-summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div>
+      <div class="introduction about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </div>
+      <div class="about-picture-background"></div>
+      <div class="about-picture"></div>
     </div>
   );
 }
