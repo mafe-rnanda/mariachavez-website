3401a39a6443de86bf82fd50d5c04d0f07273af2
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2022-10-13 16:11:30 -0700

    add gri layout to Porjects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,20 +1,29 @@
+import "./Projects.scss";
+
 export default function Work(props) {
   return (
-    <div>
-      <h1>PROJECTS</h1>
+    <div id="projects">
+      <h1 class="header">PROJECTS</h1>
       <div class="project-container">
-        <div>
-          <div>PEOJECT NAME</div>
-          <a>more details</a>
-        </div>
+        <div class="number">01</div>
+        <div class="project-name">PROJECT NAME</div>
+        <svg version="1.1" id="line_2" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"  width="80%" height="1.55em" xmlSpace="preserve"><path class="path2" stroke-width="3" stroke="#212121" d="M0 0 l1500 0"/></svg>
+        <a>more details
+          <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M9.46967 5.46967C9.76256 5.17678 10.2374 5.17678 10.5303 5.46967L16.5303 11.4697C16.8232 11.7626 16.8232 12.2374 16.5303 12.5303L10.5303 18.5303C10.2374 18.8232 9.76256 18.8232 9.46967 18.5303C9.17678 18.2374 9.17678 17.7626 9.46967 17.4697L14.9393 12L9.46967 6.53033C9.17678 6.23744 9.17678 5.76256 9.46967 5.46967Z" fill="#030D45"/>
+          </svg>
+        </a>
       </div>
       <div class="project-container">
-        <div>
-          <div>PEOJECT NAME</div>
-          <a>more details</a>
-        </div>
-      </div>
-      
+        <div class="number">02</div>
+        <div class="project-name">PROJECT NAME</div>
+        <svg version="1.1" id="line_2" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"  width="80%" height="1.5em" xmlSpace="preserve"><path class="path2" stroke-width="3" stroke="#212121" d="M0 0 l1500 0"/></svg>
+        <a>more details
+          <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M9.46967 5.46967C9.76256 5.17678 10.2374 5.17678 10.5303 5.46967L16.5303 11.4697C16.8232 11.7626 16.8232 12.2374 16.5303 12.5303L10.5303 18.5303C10.2374 18.8232 9.76256 18.8232 9.46967 18.5303C9.17678 18.2374 9.17678 17.7626 9.46967 17.4697L14.9393 12L9.46967 6.53033C9.17678 6.23744 9.17678 5.76256 9.46967 5.46967Z" fill="#030D45"/>
+          </svg>
+        </a>
+      </div>   
     </div>
   );
 }
