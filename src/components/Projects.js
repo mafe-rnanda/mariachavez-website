fed8e2505a1e2b9c3725b8273dd088224cb2234c
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2022-11-26 17:43:18 -0800

    add slideshow to project 1

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,24 +2,38 @@ import { useState } from "react";
 import "./Projects.scss";
 import arrow from '../assets/arrowRight.svg';
 import more from '../assets/moreIcon.svg';
+import helpBook1 from '../assets/helpBook/App_Dashboard.gif';
+import helpBook2 from '../assets/helpBook/Landing_Page.png';
+import helpBook3 from '../assets/helpBook/Organization_Profile.png';
+import helpBook4 from '../assets/helpBook/Donor_Profile.png';
+
 
 export default function Work(props) {
+  const helpBook =[helpBook2,helpBook3,helpBook4,helpBook1]
+  let i = 0;
+  const timer = 4000;
+
+  const helpBookSlideShow = () => {
+    document.slide.src = helpBook[i];
+    if (i < helpBook.length -1) {
+      i++;
+    } else {
+      i = 0;
+    }
+    setTimeout(helpBookSlideShow, timer);
+  };
+
   return (
     <div>
-      <div className="grid skills">
-        <h1 class="header">MY SKILLS</h1>
-        <div>Javascript, HTML/CSS, SQL, Ruby, Python</div>
-        <div>Spoken Languages: English, Spanish, French</div>
-      </div>
-      
       <div id="projects" className="grid">
       <h1 class="header">PROJECTS</h1>
 
       <div class="project-container">
         <div class="number">01</div>
-        <div class="project-name">PROJECT NAME</div>
-        <img src={more} className="showMore" />
+        <div class="project-name">HelpBook</div>
+        <img src={more} className="showMore" onClick={helpBookSlideShow} />
         <div className="divider"></div>
+        <img name="slide" className="slideShow" />
         {/* <a>more details
           <img src={arrow} />
         </a> */}
